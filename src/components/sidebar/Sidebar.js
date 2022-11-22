37275e0a5319e06dff791612eacb7602fd3e3d52
nasim67reja <nasimreja67@gmail.com>
2022-11-22 13:34:15 -0800

    Navbar Added

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -82,7 +82,10 @@ const Sidebar = () => {
           </li>
         </ul>
       </div>
-      <div className="bottom">color options</div>
+      <div className="bottom">
+        <div className="colorOption"></div>
+        <div className="colorOption"></div>
+      </div>
     </div>
   );
 };
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Navbar from "../../components/navbar/Navbar";
 import Sidebar from "../../components/sidebar/Sidebar";
 import "./home.scss";
 
@@ -6,7 +7,10 @@ const Home = () => {
   return (
     <div className="home">
       <Sidebar />
-      <div className="homeContainer">container</div>
+      <div className="homeContainer">
+        <Navbar />
+        home container
+      </div>
     </div>
   );
 };
