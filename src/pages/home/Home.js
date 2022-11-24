e70cd11f7a3dcd80484b5f2b187926f6a10d18c5
nasim67reja <nasimreja67@gmail.com>
2022-11-24 11:29:40 +0600

    Chart Component has been added succesfully 🚀

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -20,7 +20,7 @@ const Home = () => {
         </div>
         <div className="charts">
           <Featured />
-          <Chart />
+          <Chart title="Last 6 Months (Revenue)" aspect={2 / 1} />
         </div>
       </div>
     </div>
