974387b3e88e6584f535e8590ef508c37b3d7586
nasim67reja <nasimreja67@gmail.com>
2022-11-27 19:28:57 +0600

    Fetch user from Database

--- src/components/datatable/DataTable.js
+++ src/components/datatable/DataTable.js
@@ -71,7 +71,10 @@ const DataTable = () => {
       renderCell: (params) => {
         return (
           <div className="cellAction">
-            <Link to="/users/test" style={{ textDecoration: "none" }}>
+            <Link
+              to={`/users/${params.row.id}`}
+              style={{ textDecoration: "none" }}
+            >
               <div className="viewButton">View</div>
             </Link>
             <div className="deleteButton">Delete</div>
--- src/pages/single/Single.js
+++ src/pages/single/Single.js
@@ -3,8 +3,29 @@ import Sidebar from "../../components/sidebar/Sidebar";
 import Navbar from "../../components/navbar/Navbar";
 import Chart from "../../components/chart/Chart";
 import List from "../../components/table/Table";
+import { useParams } from "react-router-dom";
+import { useCallback, useEffect, useState } from "react";
+import axios from "axios";
+import { URL } from "../../App";
 
 const Single = () => {
+  const [user, setUser] = useState();
+  const params = useParams();
+
+  const getUsers = useCallback(async () => {
+    try {
+      const { data } = await axios.get(`${URL}/api/v1/users/${params.userId}`);
+      setUser(data.data.data);
+    } catch (error) {
+      console.log(`error: `, error);
+    }
+  }, [params.userId]);
+
+  useEffect(() => {
+    getUsers();
+  }, [getUsers]);
+  console.log(user);
+
   return (
     <div className="single">
       <Sidebar />
@@ -14,34 +35,37 @@ const Single = () => {
           <div className="left">
             <div className="editButton">Edit</div>
             <h1 className="title">Information</h1>
-            <div className="item">
-              <img
-                src="https://images.pexels.com/photos/733872/pexels-photo-733872.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260"
-                alt=""
-                className="itemImg"
-              />
-              <div className="details">
-                <h1 className="itemTitle">Jane Doe</h1>
-                <div className="detailItem">
-                  <span className="itemKey">Email:</span>
-                  <span className="itemValue">janedoe@gmail.com</span>
-                </div>
-                <div className="detailItem">
-                  <span className="itemKey">Phone:</span>
-                  <span className="itemValue">+1 2345 67 89</span>
-                </div>
-                <div className="detailItem">
-                  <span className="itemKey">Address:</span>
-                  <span className="itemValue">
-                    Elton St. 234 Garden Yd. NewYork
-                  </span>
-                </div>
-                <div className="detailItem">
-                  <span className="itemKey">Country:</span>
-                  <span className="itemValue">USA</span>
+            {user && (
+              <div className="item">
+                <img
+                  crossOrigin="anonymous"
+                  src={`${URL}/img/users/${user.photo}`}
+                  alt="userphoto"
+                  className="itemImg"
+                />
+                <div className="details">
+                  <h1 className="itemTitle">{user.name}</h1>
+                  <div className="detailItem">
+                    <span className="itemKey">Email:</span>
+                    <span className="itemValue">{user.email}</span>
+                  </div>
+                  <div className="detailItem">
+                    <span className="itemKey">Role:</span>
+                    <span className="itemValue">{user.role}</span>
+                  </div>
+                  <div className="detailItem">
+                    <span className="itemKey">Status:</span>
+                    <span className="itemValue">
+                      {`${user.active ? "Active" : "Deactive"}`}
+                    </span>
+                  </div>
+                  <div className="detailItem">
+                    <span className="itemKey">Country:</span>
+                    <span className="itemValue">USA</span>
+                  </div>
                 </div>
               </div>
-            </div>
+            )}
           </div>
           <div className="right">
             <Chart aspect={3 / 1} title="User Spending ( Last 6 Months)" />
